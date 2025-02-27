6edfa2003a46851e8ce1fa0163b1ffa19d8cbee4
MoniNog <nogueiramon@gmail.com>
2025-02-27 17:40:10 +0100

    MAKEFILE OOOOOOKKKKKK

---
+++ sh/minishell.h
@@ -0,0 +1,21 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "../lib/libft.h"
+
+// typedef struct s_path
+// {
+// 	char	
+// }
+
+#endif
+
+//struct def s_token
+//{
+//	int		tok;
+
+//}		t_token;
