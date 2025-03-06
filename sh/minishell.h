359375d84b16c1075fbb19bee38ce6057a9789bd
lylah <lipam.randrizzello@gmail.com>
2025-03-06 17:41:52 +0100

    commencement type/list

--- sh/minishell.h
+++ sh/minishell.h
@@ -6,70 +6,75 @@
 /*   By: lylrandr <lylrandr@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 16:41:45 by lylrandr          #+#    #+#             */
-/*   Updated: 2025/03/04 17:08:27 by lylrandr         ###   ########.fr       */
+/*   Updated: 2025/03/06 17:19:36 by lylrandr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "../lib/libft.h"
+# include <readline/history.h>
+# include <readline/readline.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-# include "../lib/libft.h"
 
-typedef enum
+typedef enum s_token_type
 {
-	CMD,
-	ARG,
-	FILE_TYPE,
-	ENV
-} WordType;
+	T_CMD,
+	T_ARG,
+	T_FILE,
+	T_ENV,
+	T_OP,
+	T_PIPE
+}					t_token_type;
 
 typedef struct s_token
 {
-	char *value;
-	WordType type;
-} t_token;
+	char			*token;
+	t_token_type	type;
+	struct token	*next;
+	struct token	*prev;
+}					t_token;
 
 // fonctions parsing
 
-char	**fill_tab(char *input, char **array);
-void	if_n_op(char *input, char **array, int *k, int *i);
-void	if_operator(char *input, char **array, int *k, int i);
-int		word_len(char *input);
-char	**first_parsing(char *input);
-char	**second_parsing(char **array);
-//void	*read_input(char **input, char **env);
-void	first_word(char **input, char **env);
-char	**parse_input(char *input);
-char	**fill_second_tab(char **array, char **tab_token);
-int		handle_non_operator(char **tab_token, char *array, int *index);
-void	handle_operator(char **tab_token, char **array, int *index, int i);
-
+char				**fill_tab(char *input, char **array);
+void				if_n_op(char *input, char **array, int *k, int *i);
+void				if_operator(char *input, char **array, int *k, int i);
+int					word_len(char *input);
+char				**first_parsing(char *input);
+char				**second_parsing(char **array);
+// void	*read_input(char **input, char **env);
+void				first_word(char **input, char **env);
+char				**parse_input(char *input);
+char				**fill_second_tab(char **array, char **tab_token);
+int					handle_non_operator(char **tab_token, char *array,
+						int *index);
+void				handle_operator(char **tab_token, char **array, int *index,
+						int i);
 
 // fonctions token
 
-int		is_cmd(char *token, char **env);
-//void	env_token(char *token);
-//void	operator_token(char *token);
-//void	word_token(char *token);
+int					is_cmd(char *token, char **env);
+// void	env_token(char *token);
+// void	operator_token(char *token);
+// void	word_token(char *token);
 
 // fonctions path
 
-char	**ft_split_path(char *fullpath);
-char	*ft_find_executable(char **path, char *cmd);
-char	*ft_get_path(char *fullpath, char *cmd);
-char	*ft_get_env_path(char **env);
+char				**ft_split_path(char *fullpath);
+char				*ft_find_executable(char **path, char *cmd);
+char				*ft_get_path(char *fullpath, char *cmd);
+char				*ft_get_env_path(char **env);
 
 // fonctions utils
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int					ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
 
-//struct def s_token
+// struct def s_token
 //{
 //	int		tok;
 
--- sh/src/tokenize.c
+++ sh/src/tokenize.c
@@ -6,7 +6,7 @@
 /*   By: lylrandr <lylrandr@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 13:28:30 by lylrandr          #+#    #+#             */
-/*   Updated: 2025/03/04 17:10:52 by lylrandr         ###   ########.fr       */
+/*   Updated: 2025/03/06 17:16:39 by lylrandr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	first_word(char **input, char **env)
 {
-	if (ft_strncmp(input[0], "<", 1) == 0 || ft_strncmp(input[0], ">",
-			1) == 0 || ft_strncmp(input[0], ">>", 2) == 0
-		|| ft_strncmp(input[0], "<<", 2) == 0)
-//		operator_token(input[0]);
+	if (ft_strncmp(input[0], "<", 1) == 0 || ft_strncmp(input[0], ">", 1) == 0
+		|| ft_strncmp(input[0], ">>", 2) == 0 || ft_strncmp(input[0], "<<",
+			2) == 0)
+		//		operator_token(input[0]);
 		printf("operateur?\n");
 	else if (!is_cmd(input[0], env))
 	{
@@ -25,35 +25,45 @@ void	first_word(char **input, char **env)
 		exit(127);
 	}
 	else
-//		word_token(input[0]);
+		//		word_token(input[0]);
 		printf("else");
 }
 
-// void	read_input(char **input, char **env)
-//{
-//	int	i;
-//
-//	i = 0;
-//	if (!input)
-//		return ;
-//	while (input[i])
-//	{
-//		if (ft_strncmp(&input[i], "<", 1) == 0 || ft_strncmp(&input[i], ">",
-//				1) == 0
-//			|| ft_strncmp(&input[i], "|", 1) == 0 || ft_strncmp(&input[i], ">>",
-//					2) == 0
-//			|| ft_strncmp(&input[i], "<<", 2) == 0)
-//			operator_token(&input[i]);
-//		else
-//		{
-//			if (input[i] == '"' || input[i] == '\'')
-//				quote_token(&input[i]);
-//			else if (input[i] == '$')
-//				env_token(&input[i]);
-//			else
-//				word_token(&input[i]);
-//		}
-//		i++;
-//	}
-//}
-//
+void	tokenize(char **input)
+{
+	int		i;
+	t_token	*tail;
+	t_token	*head;
+	t_token	*new_node;
+
+	i = 1;
+	tail = malloc(sizeof(t_token));
+	if (!tail)
+		exit(1);
+	tail->token = input[0];
+	tail->prev = NULL;
+	tail->next = NULL;
+	while (input[i])
+	{
+		new_node = malloc(sizeof(t_token));
+		if (!new_node)
+			exit(1);
+		new_node->token = input[i];
+		new_node->prev = tail;
+		new_node->next = NULL;
+		tail->next = new_node;
+		tail = new_node;
+	}
+}
+
+t_token_type	get_token_type(char *token)
+{
+	if (ft_strncmp(token, "|", 1) == 0)
+		return (T_PIPE);
+	else if (ft_strncmp(token, "<", 1) == 0 || ft_strncmp(token, ">", 1) == 0
+		|| ft_strncmp(token, ">>", 2) == 0 || ft_strncmp(token, "<<", 2) == 0)
+		return(T_OP);
+	else if (token[0] == '$')
+		return (T_ENV);
+	return (T_CMD);
+}
